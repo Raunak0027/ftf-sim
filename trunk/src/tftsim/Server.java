f41f3870bba8d2e0d80af0484073f633d34f9f1a
mastercrazychris@gmail.com <mastercrazychris@gmail.com@eb53043a-727c-d3d0-a75a-a1b209d9750d>
2010-11-03 22:25:04 +0000

    Implemented detection of deaths of known servers + Fixed/tweaked various code + added brief documentation in code.

--- trunk/src/tftsim/Server.java
+++ trunk/src/tftsim/Server.java
@@ -14,6 +14,25 @@ public class Server {
 	private ArrayList<Server> otherServers;
 	private Hashtable<String, Long> shoutsHeard;
 	
+	// deathPeriod is the period of time in milliseconds,
+	// after not hearing a shout from another server, that we
+	// determine the server to be dead.
+	//
+	// This value should be greater than the period between shouting
+	// and greater than the period between attempting to detect dead servers.
+	// You can test the death detection by setting this value less than the
+	// shoutDelay and detectDeadDelay
+	private long deathPeriod = 200;
+	
+	
+	// These values are the delay between shouting and the delay between looking
+	// for dead servers.
+	// NOTE: These values should ideally not be equal as the shouting and detecting threads
+	// will be working on the server lists at the same time.
+	private long shoutDelay = 150;
+	private long detectDeadDelay = 150;
+	
+	
 	Server(Router router, String MAC){
 		MACAddress = new String(MAC);
 		IPAddress = null;
@@ -21,7 +40,7 @@ public class Server {
 		primary = false;
 		connectedRouter = router;
 		otherServers = new ArrayList<Server>();
-		shoutsHeard = new Hashtable();
+		shoutsHeard = new Hashtable<String, Long>();
 		
 		router.addNode(this);
 	}
@@ -71,8 +90,13 @@ public class Server {
 		
 		while(shoutCount < numberOfOtherServers){
 			System.out.println("(" + this.getIP() + ") Shouting to: " + otherServers.get(shoutCount).getIP());
-			otherServers.get(shoutCount).hearShout(this.getIP());
+			try{
+				otherServers.get(shoutCount).hearShout(this.getIP());
+			}catch(Exception e){
+				
+			}
 			shoutCount++;
+			numberOfOtherServers = otherServers.size();
 		}
 		
 		
@@ -92,6 +116,38 @@ public class Server {
 		
 	}
 	
+	public void killServer(){
+		this.alive = false;
+	}
+	
+	private void removeDeads(){
+		
+		int totalOtherServers = this.otherServers.size();
+		int loopCount = 0;
+		
+		serverListLoop:
+		while(loopCount<totalOtherServers){
+			Calendar currentTime = Calendar.getInstance();
+			Long currentTimeInMillis = currentTime.getTimeInMillis();
+			
+			String mac = new String(otherServers.get(loopCount).getMAC());
+			String ip = new String(otherServers.get(loopCount).getIP());
+
+			if(currentTimeInMillis > (shoutsHeard.get(mac) + deathPeriod)){
+				System.out.println("!!!======= DEAD SERVER DETECTED =========!!!");
+				System.out.println("!!! (" + this.getIP() + ") Server: " + mac + " at " + ip + " is dead. Removing from known servers!");
+				System.out.println("!!!======================================!!!");
+				otherServers.remove(loopCount);
+				break serverListLoop;
+			}else{
+				System.out.println("(" + this.getIP() + ") ("+ currentTimeInMillis +") Server at " + ip + " not detected as dead. Last shout heard at " + shoutsHeard.get(mac));
+			}
+			
+			loopCount++;
+		}
+		
+	}
+	
 	
 	
 	
@@ -99,15 +155,41 @@ public class Server {
 	
 	public void giveLife(){
 		
+		
+		/* 
+		 * This is where we define the server's internal processes
+		 * for e.g. doShouts, removeDeadServers, etc...
+		 *
+		 * Note: when defining processes - ensure that the main code
+		 * is wrapped in "while(alive){ ... }" so that processes only
+		 * run when the server is functional. Also, within the loop,
+		 * ensure that there is at least one "Thread.sleep(...);" 
+		 * statement to avoid high CPU usage. This statement has
+		 * to be wrapped in a try/catch block.
+		 * 
+		 */
+		
 		Runnable doShouts = new Runnable() {
-
 			public void run() {
-				
 				while(alive){
 					
-					shoutOut();
 					try{
-						Thread.sleep(1000);
+						shoutOut();
+						Thread.sleep(shoutDelay);
+					}catch(Exception e){
+						// do nothing
+					}
+				}
+			}
+		};
+		
+		
+		Runnable removeDeadServers = new Runnable() {
+			public void run(){
+				while(alive){
+					try{
+						removeDeads();
+						Thread.sleep(detectDeadDelay);
 					}catch(Exception e){
 						
 					}
@@ -115,15 +197,16 @@ public class Server {
 			}
 		};
 		
-		Thread doShoutsThread = new Thread(doShouts);
 		
+		// Initialise and start threads
+		Thread doShoutsThread = new Thread(doShouts);
+		Thread removeDeadServersThread = new Thread(removeDeadServers);
 		doShoutsThread.start();
+		removeDeadServersThread.start();
 		
 	}
 	
-	public void killServer(){
-		this.alive = false;
-	}
+	
 	
 	
 }
