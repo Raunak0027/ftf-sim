2c969ada7912f63fc38e8761e2033f43479eb659
mastercrazychris@gmail.com <mastercrazychris@gmail.com@eb53043a-727c-d3d0-a75a-a1b209d9750d>
2010-11-24 14:30:39 +0000

    Minor updates

--- trunk/src/ftfsim/Client.java
+++ trunk/src/ftfsim/Client.java
@@ -16,6 +16,16 @@ public class Client {
 		
 	}
 	
+	public void testMethod(){
+		Packet packet1 = new Packet(ClientId, "SOME DESTINATION", "Packet number 1 contents", 1, 3);
+		Packet packet2 = new Packet(ClientId, "SOME DESTINATION", "Packet number 2 contents", 2, 3);
+		Packet packet3 = new Packet(ClientId, "SOME DESTINATION", "Packet number 3 contents", 3, 3);
+		
+		this.sendPacket(packet1);
+		this.sendPacket(packet2);
+		this.sendPacket(packet3);
+	}
+	
 	
 
 }
